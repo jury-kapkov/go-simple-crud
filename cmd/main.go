7bb044f6447d7f4a90fc7996bd126423265faadd
Капков Юрий <kapkov555@gmail.com>
2022-06-30 14:59:54 +0300

    добавил файл с конфигами

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/spf13/viper"
 	"log"
 	"todo"
 	"todo/pkg/handler"
@@ -9,12 +10,23 @@ import (
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("Ошибка считывания конфигов: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("Возникла ошибка запуска https сервера: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.SetConfigType("json")
+	viper.SetConfigName("config")
+	viper.AddConfigPath("../configs")
+	return viper.ReadInConfig()
 }
